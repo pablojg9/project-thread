2c2239ce00e484553faa27ffdff08378e138a753
pablojg9 <pablojuniorgn2@gmail.com>
2021-06-17 18:20:05 -0300

    Project - thread - 17/06/2021

--- src/com/thread/projeto/RunScreen.java
+++ src/com/thread/projeto/RunScreen.java
@@ -2,7 +2,7 @@ package com.thread.projeto;
 
 public class RunScreen {
     public static void main(String[] args) {
-        
+        ThreadTimeScreen threadTimeScreen = new ThreadTimeScreen();
 
 
     }
--- src/com/thread/projeto/ThreadTimeScreen.java
+++ src/com/thread/projeto/ThreadTimeScreen.java
@@ -1,7 +1,17 @@
 package com.thread.projeto;
 
-public class ThreadTimeScreen {
+import javax.swing.JDialog;
+import java.awt.*;
+
+public class ThreadTimeScreen extends JDialog {
+    public ThreadTimeScreen(){ /** Executa o que tiver dentro no momento **/
+        setTitle("Screen Thread");
+        setSize(new Dimension(550, 550));
+        setLocationRelativeTo(null);
+        setResizable(false);
 
 
 
+        setVisible(true); /** TORNA A TELA VISIVEL PARA O USUARIO **/
+    }
 }
