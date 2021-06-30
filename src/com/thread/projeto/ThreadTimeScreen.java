ac9542fa1e305a34a0ec1c880fbf179e6e2d25fe
pablojg9 <pablojuniorgn2@gmail.com>
2021-06-30 12:41:09 -0300

    Project - thread - 30/06/2021

--- src/com/thread/projeto/ThreadTimeScreen.java
+++ src/com/thread/projeto/ThreadTimeScreen.java
@@ -2,44 +2,104 @@ package com.thread.projeto;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class ThreadTimeScreen extends JDialog {
 
+    /** ATTRIBUTE JPANEL **/
     private JPanel jPanel = new JPanel(new GridBagLayout());
-    private JLabel descriptionTime = new JLabel("Time Thread 1");
-    private JTextField showTime = new JTextField();
 
-    private JLabel descriptionTimeDuo = new JLabel("Time Thread 1");
+    /** ATTRIBUTE JLABEL **/
+    private  JLabel descriptionTime = new JLabel("Time Thread 1");
+    private  JLabel descriptionTimeDuo = new JLabel("Time Thread 2");
+
+    /** ATTRIBUTE JTEXTFIELD **/
+    private final JTextField showTime = new JTextField();
     private JTextField showTimeDuo = new JTextField();
+
+    /** ATTRIBUTE BUTTON **/
+    private JButton buttonStart = new JButton("Start");
+    private JButton buttonStop = new JButton("Stop");
+
+    public Runnable threadRun = new Runnable() {
+        @Override
+        public void run() {
+            while (true) {
+                showTime.setText(new SimpleDateFormat("dd/MM/yyyy hh:mm.ss")
+                        .format(Calendar.getInstance().getTime()));
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    };
+
+    public Runnable threadStop = new Runnable() {
+        @Override
+        public void run() {
+
+
+        }
+    };
+
+
+    private Thread threadOneTime;
 
     public ThreadTimeScreen(){ /** Executa o que tiver dentro no momento **/
         setTitle("Screen Thread");
-        setSize(new Dimension(550, 550));
+        setSize(new Dimension(250, 250));
         setLocationRelativeTo(null);
         setResizable(false);
 
         GridBagConstraints gridBagConstraints = new GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.insets = new Insets(5,15,5,5);
+        gridBagConstraints.anchor = GridBagConstraints.WEST;
 
-        /** PRIMEIRA "TABELA" DE COMPONENTES **/
+        /** FIRST "TABLE" OF COMPONENTS **/
         descriptionTime.setPreferredSize(new Dimension(200, 25));
         jPanel.add(descriptionTime, gridBagConstraints);
 
         showTime.setPreferredSize(new Dimension(200, 25));
         gridBagConstraints.gridy++;
+        showTime.setEditable(false);
         jPanel.add(showTime, gridBagConstraints);
 
-        /** SEGUNDA "TABELA" DE COMPONENTES **/
+        /** SECOND "TABLE" OF COMPONENTS **/
         descriptionTimeDuo.setPreferredSize(new Dimension(200, 25));
         gridBagConstraints.gridy++;
         jPanel.add(descriptionTimeDuo, gridBagConstraints);
 
         showTimeDuo.setPreferredSize(new Dimension(200, 25));
         gridBagConstraints.gridy++;
+        showTimeDuo.setEditable(false);
         jPanel.add(showTimeDuo, gridBagConstraints);
 
+        gridBagConstraints.gridwidth = 1;
 
+        buttonStart.setPreferredSize(new Dimension(90,25));
+        gridBagConstraints.gridy++;
+        jPanel.add(buttonStart, gridBagConstraints);
+
+        buttonStop.setPreferredSize(new Dimension(90, 25));
+        gridBagConstraints.gridx++;
+        jPanel.add(buttonStop, gridBagConstraints);
+
+        buttonStart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                threadOneTime = new Thread(threadRun);
+                threadOneTime.start();
+
+            }
+        });
 
         add(jPanel, BorderLayout.WEST);
         setVisible(true); /** TORNA A TELA VISIVEL PARA O USUARIO **/
